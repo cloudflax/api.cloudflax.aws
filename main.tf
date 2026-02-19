b7a3e4006c4b0b0031254f1786be95ffbfaacb6d
Cloudflax <jose.guerrero@cloudflax.com>
2026-02-19 08:37:04 -0500

    Refactor Lambda function deployment in Terraform and remove legacy rotation logic
    - Added an archive_file data source to package the rotation Lambda function code.
    - Updated the Lambda function resource to use the new zip file output path.
    - Removed the outdated rotation.py file containing the previous Lambda function logic.

--- main.tf
+++ main.tf
@@ -85,8 +85,14 @@ resource "aws_secretsmanager_secret_version" "db_secret_version" {
 # --- ROTATION LAMBDA ---
 # --- LAMBDA DE ROTACIÓN ---
 
+data "archive_file" "rotation_lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/lambda/db_rotation"
+  output_path = "${path.module}/lambda/db_rotation/rotation_code.zip"
+}
+
 resource "aws_lambda_function" "rotation_lambda" {
-  filename      = "lambda/rotation_code.zip" 
+  filename      = data.archive_file.rotation_lambda_zip.output_path
   function_name = "tf-localstack-rotation-lambda"
   role          = aws_iam_role.lambda_role.arn
   handler       = "rotation.handler" 
@@ -94,7 +100,7 @@ resource "aws_lambda_function" "rotation_lambda" {
 
   # Hash sync to detect code changes
   # Sincronización de hash para detectar cambios en el código
-  source_code_hash = filebase64sha256("lambda/rotation_code.zip") 
+  source_code_hash = data.archive_file.rotation_lambda_zip.output_base64sha256 
 
   environment {
     variables = {
